d1364ccdb59c88b78b70e2c4aa08dd0a19ba4a44
skajihara <t15cs015@gmail.com>
2024-03-29 01:16:40 +0900

    section9-10

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { shallowRef } from 'vue'
+import { ref, shallowRef } from 'vue'
 import CompA from './components/CompA.vue'
 import CompB from './components/CompB.vue'
 import CompC from './components/CompC.vue'
 const currentComp = shallowRef(CompA)
+const isShow = ref(false)
 </script>
 <template>
   <h1>Dynamic Components</h1>
@@ -13,4 +14,13 @@ const currentComp = shallowRef(CompA)
   <KeepAlive>
     <component :is="currentComp" />
   </KeepAlive>
+  <button @click="isShow = true">Open Modal</button>
+  <Teleport to="body">
+    <dialog v-if="isShow" open>
+      <p>This is a Modal!</p>
+      <button @click="isShow = false">Close</button>
+    </dialog>
+  </Teleport>
+  <Teleport to="body"><p>A</p></Teleport>
+  <Teleport to="body"><p>B</p></Teleport>
 </template>
