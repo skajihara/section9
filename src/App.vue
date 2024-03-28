8f82c8b9d12aec2c7bc4a20747632d4fdc6c08c4
skajihara <t15cs015@gmail.com>
2024-03-29 01:01:24 +0900

    section9-7

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@ import CompA from './components/CompA.vue'
 import CompB from './components/CompB.vue'
 import CompC from './components/CompC.vue'
 const currentComp = shallowRef(CompA)
-console.log(CompA)
 </script>
 <template>
   <h1>Dynamic Components</h1>
   <button @click="currentComp = CompA">A</button>
   <button @click="currentComp = CompB">B</button>
   <button @click="currentComp = CompC">C</button>
-  <component :is="currentComp" />
+  <KeepAlive>
+    <component :is="currentComp" />
+  </KeepAlive>
+  <CompB v-show="currentComp === CompB" />
 </template>
--- src/components/CompB.vue
+++ src/components/CompB.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue'
+
+onMounted(() => {
+  console.log('CompB onMounted')
+})
+onUnmounted(() => {
+  console.log('CompB onUnmounted')
+})
+const count = ref(0)
+</script>
 <template>
-  <h2>CompB</h2>
+  <div>
+    <h2>CompB</h2>
+    <p>{{ count }}</p>
+    <button @click="count++">+1</button>
+  </div>
 </template>
